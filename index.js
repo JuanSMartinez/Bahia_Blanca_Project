8408d18b78b2614f14867bbfb411e43eb200e3ce
JuanSMartinez <js.martinez777@uniandes.edu.co>
2016-12-03 10:32:36 -0500

    reestructuracion para uso de node js

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+app.get('/vis', function(req, res){
+    res.sendFile(path.join(__dirname, '/vis.html'));
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
